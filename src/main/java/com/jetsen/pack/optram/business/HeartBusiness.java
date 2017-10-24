d58f85b772a3e83eea4f687577fb0b9351406051
dragons <jetsen02@163.com>
2017-10-25 00:11:13 +0800

    edit

--- src/main/java/com/jetsen/pack/optram/business/HeartBusiness.java
+++ src/main/java/com/jetsen/pack/optram/business/HeartBusiness.java
@@ -1,5 +1,8 @@
 package com.jetsen.pack.optram.business;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.jetsen.pack.optram.bean.RedisKey;
 import com.jetsen.pack.optram.netty.ByteOper;
 import com.jetsen.pack.optram.util.DateUtil;
 import org.apache.logging.log4j.LogManager;
@@ -10,12 +13,18 @@ import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -49,31 +58,52 @@ public class HeartBusiness {
         PlayDate.addText(playdate);
         Element SystemStatus = root.addElement("SystemStatus");
         SystemStatus.addText("0");
-
-        //通过http获取任务版本信息
+        //build request Object
         String taskNormalKey = String.format(noramlTemp,channelcode,playdate);
-        ResponseEntity<?> response = restTemplate.getForEntity("http://localhost:8080/redis/getForValue?key={key}",String.class,taskNormalKey);
-        String noramlVersion = (String) response.getBody();
-
-        //通过http获取特殊任务版本信息
         String taskSpecialKey = String.format(specialTemp,channelcode,playdate);
-        //get special version
-        ResponseEntity<?> resp = restTemplate.getForEntity("http://localhost:8080/redis/getForValue?key={key}",String.class,taskSpecialKey);
-        String specialVersion = (String) resp.getBody();
-
+        String nextDay = DateUtil.getBeforeOrAfterDay(DateUtil.str2Date(playdate,"yyyy-MM-dd"),1,"yyyy-MM-dd");
+        String taskNextDayNormalKey = String.format(noramlTemp,channelcode,nextDay);
+        String taskNextDaySpecialKey = String.format(specialTemp,channelcode,nextDay);
+        List<RedisKey> redisKeys = new ArrayList<RedisKey>();
+        RedisKey normalkey =new RedisKey(taskNormalKey,null);
+        RedisKey specialkey =new RedisKey(taskSpecialKey,null);
+        RedisKey nextnormalkey =new RedisKey(taskNextDayNormalKey,null);
+        RedisKey nextspecialkey =new RedisKey(taskNextDaySpecialKey,null);
+        redisKeys.add(normalkey);
+        redisKeys.add(specialkey);
+        redisKeys.add(nextnormalkey);
+        redisKeys.add(nextspecialkey);
+        //convert list to string of json
+        Gson gson = new Gson();
+        Type listType = new TypeToken<ArrayList<RedisKey>>(){}.getType();
+        String redisJson = gson.toJson(redisKeys,listType);
+        //http request
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json;charset=UTF-8");
+        headers.setContentType(type);
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<String> formEntity = new HttpEntity<String>(redisJson, headers);
+        ResponseEntity<String> resoEntity = restTemplate.postForEntity("http://localhost:8080/redis/getForValues",formEntity,String.class);
+        String resoBody = resoEntity.getBody();
+        //handle response body(json)
+        redisKeys = gson.fromJson(resoBody,listType);
         Element NormalPackingTodayVersion = root.addElement("NormalPackingTodayVersion");
         Element SpecialPackingTodayVersion = root.addElement("SpecialPackingTodayVersion");
-        if(!StringUtils.isEmpty(noramlVersion)){
-            NormalPackingTodayVersion.addText(noramlVersion);
+        if(!StringUtils.isEmpty(redisKeys.get(0).getValue())){
+            NormalPackingTodayVersion.addText(redisKeys.get(0).getValue());
         }
-        if(!StringUtils.isEmpty(specialVersion)){
-            SpecialPackingTodayVersion.addText(specialVersion);
+        if(!StringUtils.isEmpty(redisKeys.get(1).getValue())){
+            SpecialPackingTodayVersion.addText(redisKeys.get(1).getValue());
         }
-        String nextDay = DateUtil.getBeforeOrAfterDay(new Date(),1,"yyyy-MM-dd");
-
-//        Element NormalPackingNextDayVersion = root.addElement("NormalPackingNextDayVersion");
-//        Element SpecialPackingNextDayVersion = root.addElement("SpecialPackingNextDayVersion");
-
+        Element NormalPackingNextDayVersion = root.addElement("NormalPackingNextDayVersion");
+        Element SpecialPackingNextDayVersion = root.addElement("SpecialPackingNextDayVersion");
+        if(!StringUtils.isEmpty(redisKeys.get(2).getValue())){
+            NormalPackingNextDayVersion.addText(redisKeys.get(2).getValue());
+        }
+        if(!StringUtils.isEmpty(redisKeys.get(3).getValue())){
+            SpecialPackingNextDayVersion.addText(redisKeys.get(3).getValue());
+        }
         return document.asXML();
     }
 
